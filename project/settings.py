9930e01e5e7131125b73d702d7c17738fc75ebc1
Valeria <vabrigadina@edu.hse.ru>
2025-05-29 10:21:04 +0300

    аззазаз

--- project/settings.py
+++ project/settings.py
@@ -110,6 +110,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
+STATIC_ROOT = os.path.join(BASE_DIR , 'static')
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
